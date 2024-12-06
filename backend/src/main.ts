3279b8f7f477cd09392bf61dfc29ca9004a0880e
Adriel Avila <adrielavila@MacBook-Pro-de-Adriel.local>
2024-12-06 10:18:55 -0500

    Actualizar la ruta de archivos estáticos a 'frontend/dist' en main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,8 @@ import path from "node:path";
 const app = express();
 const port = process.env.PORT || 5176;
 
-// Servir archivos estáticos desde la carpeta 'landing/dist'
-const LANDING_DIST = path.join(__dirname, "..", "..", "landing", "dist");
+// Servir archivos estáticos desde la carpeta 'frontend/dist'
+const LANDING_DIST = path.join(__dirname, "..", "..", "frontend", "dist");
 
 app.use(express.static(LANDING_DIST));
 
